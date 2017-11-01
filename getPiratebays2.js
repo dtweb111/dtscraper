d58bd504731d98fa1dd9d9a557a363902ea6dc78
Unknown <w_zhaoming@hotmail.com>
2017-11-01 23:53:19 +1300

    zm

--- getPiratebays2.js
+++ getPiratebays2.js
@@ -1,42 +1,50 @@
 var request = require('request');
 var cheerio = require('cheerio');
-//var db = require('./db');
+var db = require('./db');
 
 
-module.exports = function (str) {
-    
-    var url = 'https://proxyship.cf/search/' + str + '/0/99/200';
+module.exports = function (data) {
+   
+    var videoName = data.video_name.trim().replace(/ /g,'%20');
+    var url = 'https://proxyship.cf/search/' + videoName + '/0/99/200';
     var name, type, size, imdb, uploaded, downloadurl;
     request(url, function (error, response, body) {
         if (body != undefined) {
             body = body.replace(/(\r\n|\n|\r)/gm, "").replace(/ +(?= )/g, '');
             $ = cheerio.load(body);
-
+            var sql = "INSERT INTO temp_piratebay (id,name,type,size,uploaded,downloadurl,video_id,video_name) VALUES ";
             $("#searchResult > tbody > tr > td > .detName > a").each(function (i1, elem1) {
                 name = $(this);
                 $("#searchResult > tbody > tr > td > font").each(function (i2, elem2) {
                     size = $(this);
                     $("#searchResult > tbody > tr > td > a[title='Download this torrent using magnet']").each(function (i3, elem3) {
                         download = $(this);
-                        if (i3 == i1 && i1 == i2) {
-                            var sql = "INSERT INTO piratebays (id,name,type,size,imdb,uploaded,downloadurl) VALUES (\""
-                            + i1 + "\",\""
-                            + name.text() + "\",\""
-                            + "Video" + "\",\""
-                            + size.text().split(",")[1] + "\",\""
-                            + "" + "\",\""
-                            + size.text().split(",")[0] + "\",\""
-                            + download.attr("href") + "\")";
-                            console.log(sql);
+                        
+                        if (i3 == i1 && i1 == i2 && download != undefined) {
+                            sql = sql + "('"
+                            + i1 + "','"
+                            + name.text() + "','"
+                            + "Video" + "','"
+                            + size.text().split(",")[1].replace('Size','').trim() + "','"
+                            + size.text().split(",")[0].replace('Uploaded','').trim() + "','"
+                            + download.attr("href") + "','"
+                            + data.id +"','"
+                            + data.video_name + "'),";
+                            console.log(name.text());
+                            //db.exec(sql, function (err, data) {
+                                //console.log(data.video_name);
+                            //});
                         }
 
                     });
                 });
 
             });
-
-
-
+            sql=sql.substring(0,sql.length-1);
+           // console.log(sql);
+            db.exec(sql, function (err, data) {
+                //console.log(err);
+            });
         }
     });
 
--- main2.js
+++ main2.js
@@ -1,4 +1,13 @@
 var piratebays2 = require('./getPiratebays2');
+var db = require('./db');
+
+var offset = 0;
+var sql = 'select id,video_name from videos where activate=1 and id<10333 order by id limit '+offset+','+30;
+db.exec(sql, null, (err, data) => {
+    for(var i=0; i < data.length; i++){
+        piratebays2(data[i]);
+    };
+});
 
 
-piratebays2(31);
+
