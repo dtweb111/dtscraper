7fc69fa8cacba3fbce573f6348cd7130e1a64777
dtweb111 <32804478+dtweb111@users.noreply.github.com>
2017-11-03 15:55:09 +1300

    Add files via upload

--- getStreamdor.js
+++ getStreamdor.js
@@ -1,50 +1,55 @@
-var request = require('request');
+//var request = require('request');
 var cheerio = require('cheerio');
-var qs = require('querystring');
-var rp = require('request-promise');
-//var db = require('./db');
-
+var db = require('./db');
+var phantom = require('phantom');
 
 module.exports = function (i) {
+
+    var _ph, _page, _outObj;
     var url;
+    var sql = "INSERT INTO temp_streamdor_pages (url) VALUES ";
+
     if (i == 1) {
-        url = 'https://www.streamdor.com/#!';
+        url = 'https://www.streamdor.com/';
     } else {
         url = 'https://www.streamdor.com/#!/?topic=&page=' + i;
     }
-    console.log(url);
-    var link;
 
-    var options = {
-        uri: 'https://www.streamdor.com/#!',
-        resolveWithFullResponse: true    //  <---  <---  <---  <---
-    };
-    
-    rp(options)
-        .then(function (body) {
-            console.log(body);
-        })
-        .catch(function (err) {
-            // Delete failed...
+    phantom
+      .create()
+      .then(ph => {
+        _ph = ph;
+        return _ph.createPage();
+      })
+      .then(page => {
+        _page = page;
+        _page.setting('javascriptEnabled').then(function(value){
+            expect(value).toEqual(true);
         });
+        return _page.open(url);
+      })
+      .then(status => {
+        //console.log(status);
+        return _page.property('content');
+      })
+      .then(content => {
+        content = content.replace(/(\r\n|\n|\r)/gm, "").replace(/ +(?= )/g, '');
+        $ = cheerio.load(content);
+        //console.log(content);
+        //console.log($("#page-content-wrapper > .movies-grid > .movie-tile > .ng-isolate-scope > div > a").first().next().attr("href"));
 
-
-
-/*
-    request(url, function (err, response, body) {
-        if (body != undefined) {
-            //body = body.replace(/(\r\n|\n|\r)/gm, "").replace(/ +(?= )/g, '');
-            $ = cheerio.load(body);
-            //console.log(body);
-            link = $("#page-content-wrapper > .movies-grid > .movie-tile > movie-tile > .movie-tile > a").attr("href");
-            
-            console.log($("#page-content-wrapper").children().length);
-            $("#page-content-wrapper > div").each(function (i1, elem1) {
-                console.log(i1);
-                console.log($(this).attr("class"));
-            });
-            
-        }
-    });
-*/
+        $("#page-content-wrapper > .movies-grid > .movie-tile > .ng-isolate-scope > div > a").each(function (i1, elem1) {
+            if($(this).attr("href")!=''){
+                sql = sql + "('"+ $(this).attr("href").replace(/\'/g, "\\'") + "'),";
+            }
+        });
+        sql=sql.substring(0,sql.length-1);
+        //console.log(sql);
+        db.exec(sql, function (err, data) {
+            console.log(err);
+        });
+        _page.close();
+        _ph.exit();
+      })
+      .catch(e => console.log(e));
 };
