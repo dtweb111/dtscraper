a3667a7e54a10626df370b7d1e22288148e17ed7
dtweb111 <32804478+dtweb111@users.noreply.github.com>
2017-11-03 15:55:16 +1300

    Add files via upload

--- main3.js
+++ main3.js
@@ -1,4 +1,8 @@
 var streamdor = require('./getStreamdor');
 //var db = require('./db');
-
+//var streamdorDetail = require('./getStreamdorDetail');
 streamdor(1);
+//var test = "abc c"+char(13)+"bbb";
+//console.log(test);
+//console.log(test.replace(/[\n\r]/g, ''));
+//streamdorDetail('https://www.streamdor.com/#!/MovieInfo/The Forbidden Kingdom?');
